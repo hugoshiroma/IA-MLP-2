0cc62272caa59e513ad222fb086dc720b7928abe
Hugo Gaio Shiroma <hshiroma@acesso.com>
2020-07-22 22:38:22 -0300

    foi

--- EP3/MainEP3.py
+++ EP3/MainEP3.py
@@ -32,16 +32,16 @@ def generate_csv():
     ionosphere_y_df = ionosphere_y_df.replace('g', 1).replace('b', 0)
     # usando holdout 60-40
     # se atentar se a cada execucao do codigo, os mesmos dados sao repartidos
-    train_df, test_df, train_targets, test_targets = train_test_split(ionosphere_x_df, ionosphere_y_df, test_size=0.4,
+    train_df, test_df, train_targets, test_targets = train_test_split(ionosphere_x_df, ionosphere_y_df, test_size=0.3,
                                                                       stratify=ionosphere_y_df)
-    train_df.to_csv('folds\\train_df.csv', index=False)
-    test_df.to_csv('folds\\test_df.csv', index=False)
-    train_targets.to_csv('folds\\train_targets.csv', index=False)
-    test_targets.to_csv('folds\\test_targets.csv', index=False)
+    train_df.to_csv('folds\\train_df.csv', index=False, header=False)
+    test_df.to_csv('folds\\test_df.csv', index=False, header=False)
+    train_targets.to_csv('folds\\train_targets.csv', index=False, header=False)
+    test_targets.to_csv('folds\\test_targets.csv', index=False, header=False)
 
 
 def init_mlp():
-    #generate_csv()
+    generate_csv()
 
     start = datetime.datetime.now()
 
@@ -49,16 +49,16 @@ def init_mlp():
     # para csv considerou o index das colunas como uma nova linha
     
     train_df = pd.read_csv('folds\\train_df.csv', header=None)
-    train_df = train_df.drop(train_df.index[0])
+    # train_df = train_df.drop(train_df.index[0])
 
     test_df = pd.read_csv('folds\\test_df.csv', header=None)
-    test_df = test_df.drop(test_df.index[0])
+    # test_df = test_df.drop(test_df.index[0])
 
     train_targets = pd.read_csv('folds\\train_targets.csv', header=None)
-    train_targets = train_targets.drop(train_targets.index[0])
+    # train_targets = train_targets.drop(train_targets.index[0])
 
     test_targets = pd.read_csv('folds\\test_targets.csv', header=None)
-    test_targets = test_targets.drop(test_targets.index[0])
+    # test_targets = test_targets.drop(test_targets.index[0])
 
 
     # estrategia baseada em grade para testar a rede MLP
